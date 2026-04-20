961f55a4329b0eecc4ecad4d941e11bd272fd769
phessophissy <adefesobi@yahoo.com>
2026-04-20 19:17:50 +0100

    feat(scripts): add reward calc part 1

---
+++ scripts/reward-calculator.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/**
+ * Reward Calculator
+ * POSVault utility script for vault monitoring and management
+ */
+
+import { execSync } from "child_process";
+
+const DEPLOYER = "SP2KYZRNME33Y39GP3RKC90DQJ45EF1N0NZNVRE09";
+const VAULT_CONTRACT = `${DEPLOYER}.vault-core-v4`;
+const API_BASE = "https://api.hiro.so";
